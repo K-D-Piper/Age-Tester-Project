3c5252327234feff390afb9eef42c48aaf9ea564
K-D-Piper <kirsten.piper@mscoding.org>
2024-09-06 15:43:58 -0500

    Finsihed First half
    finished first half of age tester

--- apps.js
+++ apps.js
@@ -13,17 +13,36 @@
     //for the idea the user would have to put in the date.
 
 let today = new Date().toLocaleDateString();
-console.log(today);    
+console.log(today); //local date
+
 //new idea. Bouncer simulator. Get in if you're 21 then black out 
     //can still use date from above. Now user needs to input their age 
 
-let userDate = document.getElementById("userDate")
-console.log(userDate)
+let testingForm = document.getElementById('myForm')
+testingForm.addEventListener("click" , (event) => {
+    event.preventDefault()
 
+}) //stops form from refeshing 
+
+let whenSubmit = document.getElementById("submit") //connects submit button to JS (now can affect it)
+
+//arrow function below 
+whenSubmit.addEventListener("click", () => {
+let userDate = document.getElementById("userDate")
+let newUserDate = userDate.value
 debugger
-//reads: if (9/5/2024 - 10/30/1998 >= 21)
-if (today - userDate >= 21){
+console.log(newUserDate)
+let userBDay = newUserDate.toLocaleDateString();
+console.log(userBDay)
+//reads: if (9/6/2024 - 10/30/1998 >= 21)
+/*
+if (today - userDate.value >= 21){
     console.log("hi")
+} else if (today - userDate.value < 21) {
+    console.log("not yet")
 } else {
-    console.log("bye")
+    console.log("im confused")
 }
+*/
+    return
+}) //arrow funciton end. Allows user's birthday to console log when clicked
