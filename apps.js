60736c249d5efae1069f5acc54e85d090b0b06d7
K-D-Piper <kirsten.piper@mscoding.org>
2024-09-06 20:02:36 -0500

    Attempted rewrite
    Moved some things

--- apps.js
+++ apps.js
@@ -1,3 +1,4 @@
+/*
 //age tester 
 
 //need:
@@ -12,17 +13,14 @@
 //above shows the date, without users input. 
     //for the idea the user would have to put in the date.
 
-let today = new Date().toLocaleDateString();
-console.log(today); //local date
+
+
+
 
 //new idea. Bouncer simulator. Get in if you're 21 then black out 
     //can still use date from above. Now user needs to input their age 
 
-let testingForm = document.getElementById('myForm')
-testingForm.addEventListener("click" , (event) => {
-    event.preventDefault()
 
-}) //stops form from refeshing 
 
 let whenSubmit = document.getElementById("submit") //connects submit button to JS (now can affect it)
 
@@ -43,6 +41,58 @@ if (today - userDate.value >= 21){
 } else {
     console.log("im confused")
 }
-*/
+
     return
 }) //arrow funciton end. Allows user's birthday to console log when clicked
+*/
+//TODAY'S DATE
+let today = new Date().toLocaleDateString();
+console.log(today); //local date
+
+
+//FORM 
+let ageForm = document.getElementById('myForm')
+
+ageForm.addEventListener("click" , (event) => 
+{
+    event.preventDefault()
+}) //stops form from refeshing 
+
+//button testing below
+
+submitInput = document.getElementById("submitInput")
+
+submitInput.addEventListener("click", () => 
+{
+    let userDate =  document.getElementById("userDate")
+    let userAge = userDate.value
+    console.log(userAge) //logs to console. Does nothing.
+
+})
+
+
+submitButton = document.getElementById("submitButton")
+console.log(submitButton) //logs nowhere
+
+//arrow function below
+/*
+let whenSubmit = document.getElementById("submit") //connects submit button to JS (now can affect it)
+
+whenSubmit.addEventListener("click", () => {
+    let userDate = document.getElementById("userDate")
+    let newUserDate = userDate.value
+    debugger
+    console.log(newUserDate)
+    let userBDay = newUserDate.toLocaleDateString();
+    console.log(userBDay)
+    //reads: if (9/6/2024 - 10/30/1998 >= 21)
+    if (today - userDate.value >= 21){
+        console.log("hi")
+    } else if (today - userDate.value < 21) {
+        console.log("not yet")
+    } else {
+        console.log("im confused")
+    }
+        return
+    }) //arrow funciton end. Allows user's birthday to console log when clicked
+*/
